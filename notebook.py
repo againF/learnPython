35c9a942fb3831276ae1bd2bb762bd670daec673
Wu YeFei <223056884@gehealthcare.com>
2024-12-05 15:40:06 +0800

    feat: 删除指定笔记

--- notebook.py
+++ notebook.py
@@ -5,13 +5,20 @@ def show_menu():
     print("2. 查看所有笔记")
     print("3. 删除所有笔记")
     print("4. 搜索笔记")
+    print("5. 删除指定笔记")
     print("0. 退出程序")
 def add_note():
     note = input("请输入新的笔记内容: ")
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        with open('notes.txt','r') as file:
+            notes = file.readlines()
+            note_id = len(notes) +1
+    except FileNotFoundError:
+        note_id = 1
     with open('notes.txt', 'a') as file:
-        file.write(f"[{timestamp}] {note}\n")
-    print("笔记已成功添加.")
+        file.write(f"{note_id}.[{timestamp}] {note}\n")
+    print(f"笔记已成功添加.(编号{note_id})")
 def view_all_notes():
     try:
        with open('notes.txt','r') as file:
@@ -28,6 +35,24 @@ def delete_all_notes():
     with open('notes.txt', 'w'):
         pass
     print("所有笔记已被清空.")
+def delete_note_by_id():
+    try:
+        with open('notes.txt','r') as file:
+            notes = file.readlines()
+        if not notes:
+            print("没有任何笔记.")
+            return
+        view_all_notes()
+        note_id = input("请输入要删除的笔记编号: ")
+        notes = [note for note in notes if not note.startswith(f"{note_id}.")]
+        with open('notes.txt', 'w') as file:
+            file.writelines(notes)
+        print(f"笔记{note_id}已成功删除.")
+    except FileNotFoundError:
+        print('尚无笔记记录.')
+
+
+
 def search_notes():
     keyword = input("请输入关键字: ")
     try:
@@ -47,7 +72,7 @@ def search_notes():
 def main():
     while True:
         show_menu()
-        choice = input("\n请选择操作 (0/1/2/3/4): ")
+        choice = input("\n请选择操作 (0/1/2/3/4/5): ")
         if choice == '1':
             add_note()
         elif choice == '2':
@@ -56,6 +81,8 @@ def main():
             delete_all_notes()
         elif choice == '4':
             search_notes()
+        elif choice == '5':
+            delete_note_by_id()
         elif choice == '0':
             break
         else:
