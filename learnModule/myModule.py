8e3010657ea53fdb756bc5dc3cafd7d5509432c6
Alan <wyffcld@163.com>
2024-12-08 15:54:31 +0800

    feat: test

--- learnModule/myModule.py
+++ learnModule/myModule.py
@@ -2,3 +2,8 @@ def greet(name):
     print(f"Hello, {name}!")
 def multiply(a, b):
     return a * b
+
+if __name__ == "__main__":
+    greet("Alice")
+    result = multiply(2, 3)
+    print(result)
