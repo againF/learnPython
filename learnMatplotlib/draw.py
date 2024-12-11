7fccd39d31179d729546a96ee812648e2aab58a6
Alan <wyffcld@163.com>
2024-12-11 21:00:19 +0800

    feat: 画柱状图

--- learnMatplotlib/draw.py
+++ learnMatplotlib/draw.py
@@ -9,4 +9,13 @@ plt.xlabel("x轴")
 
 plt.ylabel("y轴")
 plt.legend()
+# plt.show()
+
+categories = ['A', 'B', 'C','D']
+values = [25, 30, 15, 20]
+plt.bar(categories, values,color="red")
+plt.title("柱状图")
+plt.xlabel("类别")
+plt.ylabel("值")
+
 plt.show()
