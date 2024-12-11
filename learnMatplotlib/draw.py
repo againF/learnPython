d93bd5a154854a1ffad74ef4abede7cdec7d010b
Alan <wyffcld@163.com>
2024-12-11 20:57:53 +0800

    feat: 画折线图

---
+++ learnMatplotlib/draw.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4,5]
+y = [1, 4, 9, 16, 25]
+
+plt.plot(x, y,label='y = 2x',color="red",marker="o")
+plt.title("折线图")
+plt.xlabel("x轴")
+
+plt.ylabel("y轴")
+plt.legend()
+plt.show()
